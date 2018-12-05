7e11bb74a7d25a14b4347e56e3bc4e63dfd8a23f
Dan Monaghan <dan@daniel-monaghan.co.uk>
2018-12-05 00:30:18 +0000

    - Updated message to describe which commands are being executed

--- slack/deploy.php
+++ slack/deploy.php
@@ -3,7 +3,10 @@ header('Content-Type: application/json');
 $response = [
 
 	'response_type' => 'in_channel',
-	'text' => $_POST['user_name'] . ' began deployment on docker2.aberfitness.biz, please wait...'
+	'text' => $_POST['user_name'] . ' began deployment on docker2.aberfitness.biz, please wait...',
+	'attachments' => [
+		['text' => 'Running `cd /shared/deployment && git pull && docker-compose down && docker-compose pull && docker-compose up -d`']
+	]
 ];
 
 $response = json_encode($response);
